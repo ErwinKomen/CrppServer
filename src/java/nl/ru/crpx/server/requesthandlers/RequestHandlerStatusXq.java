f245b1504cbd3329066c1fa6114b3803db7d37b6
ErwinKomen <E.Komen@ru.nl>
2018-03-14 09:49:50 +0100

    Extended /statusxq to include a list

--- src/java/nl/ru/crpx/server/requesthandlers/RequestHandlerStatusXq.java
+++ src/java/nl/ru/crpx/server/requesthandlers/RequestHandlerStatusXq.java
@@ -7,6 +7,7 @@ package nl.ru.crpx.server.requesthandlers;
 
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import nl.ru.crpx.dataobject.DataFormat;
 import nl.ru.crpx.dataobject.DataObject;
 import nl.ru.crpx.dataobject.DataObjectMapElement;
 import nl.ru.crpx.search.Job;
@@ -58,55 +59,63 @@ public class RequestHandlerStatusXq  extends RequestHandler {
       // Get the status for this Xq job from this user
       DataObjectMapElement objContent = new DataObjectMapElement();
       objContent.put("jobid", sStatusJobId);
-      // Get a handle to the Xq job
-      Job search = searchMan.searchGetJobXq(sStatusJobId);
-      // Validate
-      if (search == null) {
-        logger.debug("Cannot find job #" + sStatusJobId + " for user [" + sStatusUserId + "]");
-        return DataObject.errorObject("INTERNAL_ERROR", 
-              "Cannot find job #" + sStatusJobId + " for user [" + sStatusUserId + "]");
-      }
-      // TODO: check if the indicated @userid has had anything to do with this job...
       
-      // Action depends on the current status of the job
-      if (search.finished()) {
-        String sJobStatus = search.getJobStatus();
-        sCode = sJobStatus;
-        // Check if the status is error
-        if (sJobStatus.equals("error")) {
-          // Get the error message
-          sResult = this.errorCollect(search);
-        } else {
-          sResult = "The search has finished";
-          objContent.put("searchParam", searchParam.toDataObject());
-          objContent.put("searchTime", search.executionTimeMillis());
-          objContent.put("searchDone", search.finished());
-          objContent.put("query", search.getJobQuery());        // The 'query' contains the original request parameters
-          objContent.put("taskid", search.getJobTaskId());
-          objContent.put("table", search.getJobTable());        // PROBLEM: this table can become very large...
-          // Take over SOME values from the JobProgress
-          JSONObject oProg = search.getJobProgress();
-          objContent.put("total", oProg.getInt("total"));       // Total number of files processed
-        }
+      // Check if the user asks for a list or a specific (numeric) job id
+      if (sStatusJobId.equals("list")) {
+        // Get a list and put it in the content
+        objContent.put("list", searchMan.jobList());
       } else {
-        sCode = "working";
-        sResult = "please wait";
-        JSONObject oProg = search.getJobProgress();
-        String sStatusStart = oProg.has("start") ? oProg.getString("start") : "-";
-        String sStatusFinish = oProg.has("finish") ? oProg.getString("finish") : "-";
-        objContent.put("start", sStatusStart);    // Name of most recently started file
-        objContent.put("finish", sStatusFinish);  // Name of most recently finished file
-        int iStatusCount = oProg.has("count") ? oProg.getInt("count") : 0;
-        int iStatusTotal = oProg.has("total") ? oProg.getInt("total") : 0;
-        int iStatusReady = oProg.has("ready") ? oProg.getInt("ready") : 0;
-        int iStatusFound = oProg.has("found") ? oProg.getInt("found") : 0;
-        objContent.put("count", iStatusCount);       // Number of files that have started
-        objContent.put("total", iStatusTotal);       // Total number of files to be done
-        objContent.put("ready", iStatusReady);       // Number of files ready
-        objContent.put("found", iStatusFound);       // Number of hits total found
+        // Get a handle to the Xq job
+        Job search = searchMan.searchGetJobXq(sStatusJobId);
+        // Validate
+        if (search == null) {
+          logger.debug("Cannot find job #" + sStatusJobId + " for user [" + sStatusUserId + "]");
+          logger.debug("Overview of current jobs:");
+          logger.debug( searchMan.jobList().toString(DataFormat.JSON) );
+          return DataObject.errorObject("INTERNAL_ERROR", 
+                "Cannot find job #" + sStatusJobId + " for user [" + sStatusUserId + "]");
+        }
+        // TODO: check if the indicated @userid has had anything to do with this job...
+
+        // Action depends on the current status of the job
+        if (search.finished()) {
+          String sJobStatus = search.getJobStatus();
+          sCode = sJobStatus;
+          // Check if the status is error
+          if (sJobStatus.equals("error")) {
+            // Get the error message
+            sResult = this.errorCollect(search);
+          } else {
+            sResult = "The search has finished";
+            objContent.put("searchParam", searchParam.toDataObject());
+            objContent.put("searchTime", search.executionTimeMillis());
+            objContent.put("searchDone", search.finished());
+            objContent.put("query", search.getJobQuery());        // The 'query' contains the original request parameters
+            objContent.put("taskid", search.getJobTaskId());
+            objContent.put("table", search.getJobTable());        // PROBLEM: this table can become very large...
+            // Take over SOME values from the JobProgress
+            JSONObject oProg = search.getJobProgress();
+            objContent.put("total", oProg.getInt("total"));       // Total number of files processed
+          }
+        } else {
+          sCode = "working";
+          sResult = "please wait";
+          JSONObject oProg = search.getJobProgress();
+          String sStatusStart = oProg.has("start") ? oProg.getString("start") : "-";
+          String sStatusFinish = oProg.has("finish") ? oProg.getString("finish") : "-";
+          objContent.put("start", sStatusStart);    // Name of most recently started file
+          objContent.put("finish", sStatusFinish);  // Name of most recently finished file
+          int iStatusCount = oProg.has("count") ? oProg.getInt("count") : 0;
+          int iStatusTotal = oProg.has("total") ? oProg.getInt("total") : 0;
+          int iStatusReady = oProg.has("ready") ? oProg.getInt("ready") : 0;
+          int iStatusFound = oProg.has("found") ? oProg.getInt("found") : 0;
+          objContent.put("count", iStatusCount);       // Number of files that have started
+          objContent.put("total", iStatusTotal);       // Total number of files to be done
+          objContent.put("ready", iStatusReady);       // Number of files ready
+          objContent.put("found", iStatusFound);       // Number of hits total found
+        }
       }
       
-    
       // Prepare a status object to return
       DataObjectMapElement objStatus = new DataObjectMapElement();
       objStatus.put("code", sCode);
